174ffaab4072875784b24ac4582d076e0a743869
korneliarohulko <korneliarohulko@gmail.com>
2018-12-28 21:48:57 +0100

    Initial files

---
+++ MembershipsApp/MembershipsApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MembershipsApp.Startup))]
+namespace MembershipsApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
